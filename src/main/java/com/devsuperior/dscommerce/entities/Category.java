ffe0d4f00bae1d1a6f958b2a6ab69e981cc08586
AndreiFulcher <andreii.fulcher@gmail.com>
2026-01-11 21:38:47 -0300

    feat: add product-order relationship

--- src/main/java/com/devsuperior/dscommerce/entities/Category.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Category.java
@@ -1,9 +1,12 @@
 package com.devsuperior.dscommerce.entities;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_category")
 public class Category {
 
     @Id
@@ -11,4 +14,34 @@ public class Category {
     private long id;
     private String name;
 
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
+
+    public Category() {
+    }
+
+    public Category(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
 }
