23982c1fe673d1d0b1873db5ca123b957353c19b
AndreiFulcher <andreii.fulcher@gmail.com>
2026-01-15 21:32:41 -0300

    feat: Exception handling

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,11 +1,14 @@
 package com.devsuperior.dscommerce.controllers;
 
+import com.devsuperior.dscommerce.dto.CustomError;
 import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.services.ProductService;
+import com.devsuperior.dscommerce.services.exceptions.ResourseNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -3,10 +3,15 @@ package com.devsuperior.dscommerce.services;
 import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
+import com.devsuperior.dscommerce.services.exceptions.DatabaseException;
+import com.devsuperior.dscommerce.services.exceptions.ResourseNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service // Serve para registrar a classe como um componente de serviço no Spring
@@ -17,7 +22,8 @@ public class ProductService {
 
     @Transactional(readOnly = true) // Serve para definir o escopo da transação como somente leitura
     public ProductDTO findById(Long id) {
-        Product product = repository.findById(id).get();
+        Product product = repository.findById(id).orElseThrow(
+                () -> new ResourseNotFoundException("Recurso não encontrado")); // Busca o produto pelo ID no repositório, lançando uma exceção se não for encontrado
         return new ProductDTO(product);
     }
 
@@ -37,9 +43,27 @@ public class ProductService {
 
     @Transactional
     public ProductDTO update(Long id, ProductDTO dto) { // Serve para inserir um novo produto no banco de dados
-        Product entity = repository.getReferenceById(id); // Obtém uma referência à entidade do produto com o ID fornecido
-        copyDtoToEntity(dto, entity); // Copia os dados do DTO para a entidade
-        return new ProductDTO(entity); // Retorna o DTO do produto atualizado
+        try{
+            Product entity = repository.getReferenceById(id); // Obtém uma referência à entidade do produto com o ID fornecido
+            copyDtoToEntity(dto, entity); // Copia os dados do DTO para a entidade
+            entity = repository.save(entity); // Salva a entidade do produto no repositório
+            return new ProductDTO(entity); // Retorna o DTO do produto atualizado
+        } catch (EntityNotFoundException e) {
+            throw new ResourseNotFoundException("Recurso não encontrado");
+        }
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id) {
+        if (!repository.existsById(id)) {
+            throw new ResourseNotFoundException("Recurso não encontrado");
+        }
+        try {
+            repository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Falha de integridade referencial");
+        }
     }
 
     private void copyDtoToEntity(ProductDTO dto, Product entity) {
@@ -47,10 +71,5 @@ public class ProductService {
         entity.setDescription(dto.getDescription());
         entity.setPrice(dto.getPrice());
         entity.setImgUrl(dto.getImgUrl());
-    }
-
-    @Transactional
-    public void delete(Long id) {
-        repository.deleteById(id);
     }
 }
