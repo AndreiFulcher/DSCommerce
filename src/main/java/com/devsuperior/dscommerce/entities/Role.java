a6137e6551a65d2586a0917082e89f72ca452606
Andrei Fulcher Ribeiro <andreii.fulcher@gmail.com>
2026-02-22 17:57:46 -0300

    UserDetails and GrantedAuthority

--- src/main/java/com/devsuperior/dscommerce/entities/Role.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Role.java
@@ -1,25 +1,25 @@
 package com.devsuperior.dscommerce.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
 
 @Table(name = "tb_role")
 @Entity
-public class Role {
+public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String authority;
 
-    public Role(){
-
-    }
+    public Role(){}
 
     public Role(Long id, String authority) {
         this.id = id;
         this.authority = authority;
     }
 
+    @Override
     public String getAuthority() {
         return authority;
     }
